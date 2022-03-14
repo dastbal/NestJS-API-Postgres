1c5257f4b912718068658951152f787b680dc22b
dastball <you@example.com>
2022-03-14 16:54:28 -0500

    deploying to heroku

--- src/app.module.ts
+++ src/app.module.ts
@@ -22,13 +22,6 @@ import config from './config';
       isGlobal: true,
       validationSchema: Joi.object({
         API_KEY: Joi.string().required(),
-        DATABASE_NAME: Joi.string().required(),
-        DATABASE_PORT: Joi.number().required(),
-        POSTGRES_PORT: Joi.number().required(),
-        POSTGRES_DB: Joi.string().required(),
-        POSTGRES_USER: Joi.string().required(),
-        POSTGRES_PASSWORD: Joi.string().required(),
-        POSTGRES_HOST: Joi.string().required(),
         DATABASE_URL: Joi.string().required(),
       }),
     }),
